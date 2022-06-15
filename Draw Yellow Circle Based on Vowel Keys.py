89292189ad8ca6356c84de0def7f5fa7c83c72e8
Verisimilitude <acharyapiyush1@outlook.com>
2022-06-15 15:26:27 -0700

    Update Draw Yellow Circle Based on Vowel Keys.py

--- Draw Yellow Circle Based on Vowel Keys.py
+++ Draw Yellow Circle Based on Vowel Keys.py
@@ -1,22 +1,13 @@
-"""
-LESSON: RPS Wars Project
-WARMUP 1
-"""
-
 import random
 import pygame
 pygame.init()
 
-# DRAW CIRCLE FUNCTION
-# Change this so that the color is yellow by default
 def draw_circle(window, color=(255, 255, 255)):
     x = random.randint(0, 500)
     y = random.randint(0, 500)
     size = random.randint(20, 30)
     pygame.draw.circle(window, color, (x, y), size)
 
-
-# MAIN PROGRAM
 w = pygame.display.set_mode([500, 500])
 w.fill((0, 0, 0))
 
@@ -27,8 +18,7 @@ while drawing:
             drawing = False
 
         if event.type == pygame.KEYDOWN:
-
-            # Draw yellow on vowels, white otherwise
+            
             if event.key == pygame.K_a:
                 draw_circle(w, (255, 255, 0))
             elif event.key == pygame.K_e:
@@ -42,6 +32,4 @@ while drawing:
             else:
                 draw_circle(w, (255, 255, 255))
 
-
     pygame.display.flip()
-
